1dfc64a855d324ce6b32933f0cd49130237b4d28
Vadim <wazzupvadim@gmail.com>
2020-10-28 22:11:47 +0200

    added hm8

---
+++ js/script.js
@@ -0,0 +1,25 @@
+let input = document.getElementById('1');
+let error = document.querySelector('.error-message')
+
+
+input.addEventListener("focusout" , function (e) {
+    if (input.value >0) {
+        let span = document.createElement('span');
+        span.innerHTML = `current price: ${input.value} <img class="button-close" src="img/close.png">`;
+        input.style.color = "green";
+        document.body.insertBefore(span, document.querySelector('.input-container'));
+        let buttonClose = document.querySelector('.button-close');
+        buttonClose.addEventListener('click', function () {
+            span.style.display = "none"
+            input.value = ""
+        })
+        input.style.border = "2px solid green";
+        error.innerHTML= "";
+    } else {
+        error.innerHTML = 'Please enter correct price.';
+        input.style.border = "2px solid red";
+        input.value = "";
+    }
+
+
+})
