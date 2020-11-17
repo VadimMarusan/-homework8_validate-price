8c27dfd1d141356058f7f9b28d5f009a41cb97e9
Vadim <wazzupvadim@gmail.com>
2020-11-17 22:54:52 +0200

    fixed error

--- js/script.js
+++ js/script.js
@@ -5,13 +5,14 @@ span.innerHTML = "";
 
 
 input.addEventListener("focusout" , function (e) {
+
     if(input.value > 0) {
         span.innerHTML = `current price: ${input.value} <img class="button-close" src="img/close.png">`;
         input.style.color = "green";
         document.body.insertBefore(span, document.querySelector('.input-container'));
         let buttonClose = document.querySelector('.button-close');
         buttonClose.addEventListener('click', function () {
-            span.style.display = "none"
+            span.innerHTML = ""
             input.value = ""
         })
         input.style.border = "2px solid green";
